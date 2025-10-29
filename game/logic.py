040ca60e1ebf6b21d352b864b4bf4325a033dc4d
yossi lerer <ylerer770@gmail.com>
2025-10-29 13:16:39 +0200

    add all function from logic

--- game/logic.py
+++ game/logic.py
@@ -12,3 +12,46 @@ def score_guess(secret: str, guess: str) -> tuple[int, int]:
             cows += 1
 
     return (bull, cows)
+
+
+def is_won(bulls: int, length: int) -> bool:
+    if bulls == length:
+        return True
+
+
+def init_state(secret: str, length: int, max_tries: int | None, unique_digits: bool, allow_leading_zero: bool):
+
+    game_state = {
+        "secret" : secret,
+        "length" : length,
+        "max_tries" : max_tries,
+        "tries_used" : 0,
+        "unique_digits" : unique_digits,
+        "allow_leading_zero" : allow_leading_zero,
+        "history" : [],
+        "seen" : set()}
+
+    return game_state
+
+
+def apply_guess(state: dict, guess: str) -> tuple[int, int]:
+
+
+    score1, score2 = score_guess(state["secret"], guess)
+    state["tries_used"] += 1
+    state["seen"].add(guess)
+    state["history"].append(tuple(list((guess, score1, score2))))
+
+    return tuple((score1,score2))
+
+
+
+
+
+
+
+
+
+
+
+
